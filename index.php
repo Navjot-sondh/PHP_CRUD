a1773e2cb2dcc3ed3e3027f1b7c945d4f2f5782f
Navjot <navjot.singh2472017@gmail.com>
2019-08-14 15:27:58 +0530

    First commit

---
+++ index.php
@@ -0,0 +1,15 @@
+<doctype html>
+    <html>
+        <head>
+
+        </head>
+        <body>
+            <form action="" method="post">
+                <label>Name</label>
+                <input type="text" name="name" value="Enter your name">
+                <label>Location</label>
+                <input type="text" name="location" value="Enter your Location">
+                <button type="submit" name="save">Save</button>
+            </form>
+        </body>
+    </html>
